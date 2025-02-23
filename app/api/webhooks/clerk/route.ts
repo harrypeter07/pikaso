02043bbbfe6e12fb53ec3b8e2975a1e66a8bc056
harrypeter07 <hassanmansuri570@gmail.com>
2025-02-23 23:41:18 +0530

    h

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -173,6 +173,9 @@ export async function POST(req: Request) {
   // For this guide, log payload to console
   const { id } = evt.data
   const eventType = evt.type
+  if(eventType === 'user.created'){
+    alert("hello")
+  }
   console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
   console.log('Webhook payload:', body)
 
