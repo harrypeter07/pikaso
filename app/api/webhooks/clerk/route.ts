3d48155727d0dcc3e140999de82f153733951ad4
harrypeter07 <hassanmansuri570@gmail.com>
2025-02-24 00:00:12 +0530

    ji

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,7 +2,7 @@
 // import { headers } from 'next/headers'
 // import { WebhookEvent } from '@clerk/nextjs/server'
 // import { clerkClient } from "@clerk/nextjs/server"
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions"
+import { createUser } from "@/lib/actions/user.actions"
 
 // export async function POST(req: Request) {
 //   // Get the SIGNING_SECRET (renamed from WEBHOOK_SECRET)
